995eb9d6d8d89fed728cfd6d1af17128a07834c2
kjungw <sagakkb@gmail.com>
2025-06-26 15:55:12 +0900

    init project

---
+++ src/test/java/clcd/practice/jenkins/JenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package clcd.practice.jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
